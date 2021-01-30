ea0337db5b84e528c8e77f03629776e053eb34a2
ThoshHub <shamthosh@gmail.com>
2021-01-30 00:52:34 -0500

    Registered models and fixed git problems (I think)

--- bugtracker/admin.py
+++ bugtracker/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, organization, bug
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin): 
+    model = User
+admin.site.register(User, UserAdmin)
+admin.site.register(organization) # 
+admin.site.register(bug)
